6ffaf3c81217a041fea13cc2b0df7c25e3197743
123564 <52309465+Lfninja@users.noreply.github.com>
2023-03-24 14:49:47 +0800

    Update dome.py

--- dome.py
+++ dome.py
@@ -1,1 +1,12 @@
-print('Hello World!')
+#!/usr/bin/env python
+# coding=utf-8
+
+class Dome(object):
+
+    @staticmethod
+    def domes():
+        print('Hello World!')
+
+
+if __name__ == "__main__":
+    Dome().domes()
